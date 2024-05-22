6f5442b552d51bf717245ab792f884c73c3de3a9
Jessico Holzmann <jesstholzmann@gmail.com>
2024-05-22 20:32:45 -0300

    membrecia de gimnasio

---
+++ index.js
@@ -0,0 +1,15 @@
+let dos = 15000,
+tres = 18000,
+week = 20000,
+ocho = "Profesor Pablo Diaz";
+diez = "Profesor Adrian Lopez";
+dieciseis = "Profesora Sofia Garcia";
+
+membrecia = prompt (`
+    ingrese sus datos y preferencias para comenzar membrecia:
+    1. Elija cuantos dias semanales comenzara
+    2. Elija horario de disponibilidad
+    3. Ingresar nombre y apellido
+    4. Fechas disponibles
+    5. Terminar inscripcion
+    `);
